32fc6c28e7c702cf32458caef6e0a3827d0b6b22
Maksym Cherniavskyi <morfey897@gmail.com>
2023-05-03 22:41:25 +0300

    Changed

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,8 +1,6 @@
 
-export const config = {
-	runtime: 'edge',
-};
+export const runtime = 'edge';
 
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+	return new Response('Hello, Next.js!')
 }
