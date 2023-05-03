84f4bece91e705a37bf89495bb1cb72110c57aa6
Maksym Cherniavskyi <morfey897@gmail.com>
2023-05-03 23:05:17 +0300

    New dir

--- src/app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-
-export const runtime = 'experimental-edge';
-
-export async function GET(request: Request) {
-	return new Response('Hello, Next.js!')
-}
---
+++ src/pages/api/hello.ts
@@ -0,0 +1,20 @@
+
+import type { NextRequest } from 'next/server';
+
+export const config = {
+	runtime: 'edge',
+};
+
+export default async function handler(req: NextRequest) {
+	return new Response(
+		JSON.stringify({
+			name: 'Test name',
+		}),
+		{
+			status: 200,
+			headers: {
+				'content-type': 'application/json',
+			},
+		}
+	);
+}
