75178da80cc8c9bc40e272cefa529e0c6a9c20f1
Maksym Cherniavskyi <morfey897@gmail.com>
2023-05-08 11:44:54 +0300

    Testing

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,11 +1,13 @@
 
 import type { NextRequest } from 'next/server';
+import type { KVNamespace, D1Database } from "@cloudflare/workers-types";
 
 export const config = {
 	runtime: 'edge',
 };
 
 export default async function handler(req: NextRequest) {
+	const db = (process.env.D1_TEST_DB as unknown) as D1Database;
 	return new Response(
 		JSON.stringify({
 			name: process.env.SOME_KEY,
